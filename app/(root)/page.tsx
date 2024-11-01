dea8f92876dc5e1b708f1eb6f6a2834d8597ba76
SaiRakindi <106252250+SaiRakindiAL@users.noreply.github.com>
2024-11-01 22:06:37 +0530

    feat: hero section and search bar implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import SearchForm from "../components/SearchForm";
 
-const Page = () => {
-  return <div>Home</div>;
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
 };
 
-export default Page;
+export default Home;
